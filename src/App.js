2d82adf0cfd9ee326a161b484b1a3b6a4babeaf8
Max Lewis <max.a.lewis98@gmail.com>
2020-05-13 13:40:57 -0700

    fully functional, just not pretty

--- src/App.js
+++ src/App.js
@@ -39,17 +39,35 @@ class App extends Component {
 
   gameFunction(e, name) {
     e.preventDefault()
-    console.log(name)
 
     let guess = true
     for (var i = 0; i < this.state.recorded.length; i++) {
-      console.log(this.state.recorded[i])
       if (name === this.state.recorded[i]) { guess = false }
     }
-    if(!guess){
-      // reset game defeat
-    } else {
+    if (!guess) {
+      let shuffle = this.state.images
+      for (let i = shuffle.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffle[i], shuffle[j]] = [shuffle[j], shuffle[i]];
+      }
 
+      this.setState({ score: 0, recorded: [], message: "Game OVER", images: shuffle})
+
+    } else {
+      let allRecord = this.state.recorded
+      allRecord.push(name)
+
+      let newScore = this.state.score + 1
+      let newRecord = this.state.topScore
+      if (newScore > newRecord) { newRecord++ }
+
+      let shuffle = this.state.images
+      for (let i = shuffle.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffle[i], shuffle[j]] = [shuffle[j], shuffle[i]];
+      }
+
+      this.setState({ score: newScore, recorded: allRecord, message: "Correct!", images: shuffle, topScore: newRecord })
     }
   }
 
