9928bbf4b51a7143e5f63c7345d25a9bc8cc8e7b
Max Lewis <max.a.lewis98@gmail.com>
2020-05-14 13:27:48 -0700

    attempt 20

--- server.js
+++ server.js
@@ -1,38 +1,25 @@
-// // app.use(express.static(publicPath));
-// // app.use('/static', express.static(path.join(__dirname, 'public')));
-
-// app.use(express.static(path.join(__dirname, 'public')));
-
-
-// app.listen(port, () => {
-//     console.log('localhost' + port);
-// });
-
-
-
 const express = require("express");
-const path = require("path")
-
+const router = require("express").Router();
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-
-app.use(express.static(path.join(__dirname, 'build')));
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-})
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
 
 // Start the API server
-app.listen(PORT, function () {
+app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
