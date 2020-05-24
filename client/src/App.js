c4fc0f4b4b6c909cf6cbf0c22541b0262179df35
Max Lewis <max.a.lewis98@gmail.com>
2020-05-24 15:28:12 -0700

    flexbox working

--- client/src/App.js
+++ client/src/App.js
@@ -12,39 +12,6 @@ class App extends Component {
     topScore: 0,
     message: "Click to begin",
     recorded: [],
-    cards: [
-      // {
-      //   name: "tarot-chariot",
-      //   // img: chariot
-      // }
-    ]
-    // images: [
-    //   {
-    //     name: "O'Neill",
-    //     link: "./O'Neill.jpg",
-    //   }, {
-    //     name: "Carter",
-    //     link: "./Carter.jpg",
-    //   }, {
-    //     name: "Jackson",
-    //     link: "./Jackson.jpg",
-    //   }, {
-    //     name: "Teal'c",
-    //     link: "./Teal'c.jpg",
-    //   }, {
-    //     name: "Sheppard",
-    //     link: "./Sheppard.jpg",
-    //   }, {
-    //     name: "Teyla",
-    //     link: "./Teyla.jpg",
-    //   }, {
-    //     name: "Ronon",
-    //     link: "./Ronon.jpg",
-    //   }, {
-    //     name: "Mckay",
-    //     link: "./Mckay.jpg",
-    //   },
-    // ]
   }
 
   // gameFunction(e, name) {
@@ -84,38 +51,11 @@ class App extends Component {
 
   render() {
     return (
-      // <div className="App">
-      //   <header className="App-header">
-      //     <h3>Clicky Game!</h3>
-      //     <p>
-      //       Click on an image to earn points, but don't click on any more than once!
-      //   </p>
-      //     <h4>\|/</h4>
-      //   </header>
-      //   <div className="App-header blue">
-      //     <div className="aqua p-5">
-      //       <form className="form-inline">
-      //         <h2 className="p-2">Score: {this.state.score}</h2>
-      //         <h2 className="p-2">Top Score: {this.state.topScore}</h2>
-      //         <h2 className="p-2">{this.state.message}</h2>
-      //       </form>
-      //     </div>
-      //     <div className="d-flex flex-wrap">
-      //       {this.state.images.map((res, i) => (
-      //         <div key={i} className="border p-2 m-5" onClick={e => this.gameFunction(e, res.name)}>
-      //           <img src={res.link || "..//placeholder.png"} alt=" " />
-      //         </div>
-      //       ))}
-      //     </div>
-      //   </div>
-      // </div>
       <div>
         <Navbar />
-        <div>
+        <div className="d-inline-flex">
           {images.map((card) => (
-            <div key={card.id}>
-              < TarrotCard {...card} />
-            </div>
+            <TarrotCard {...card} key={card.id} />
           ))}
         </div>
       </div>
--- client/src/components/TarrotCard.js
+++ client/src/components/TarrotCard.js
@@ -3,9 +3,7 @@ import React from 'react';
 function TarrotCard(props) {
 
     return (
-        <div class="border p-1 m-1">
-            <img src={props.src} alt={props.name}/>
-        </div>
+        <img src={props.src} alt={props.name}/>
     )
 }
 
