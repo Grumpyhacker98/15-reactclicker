901f1866f47bb481dbefeee68accf185e126876c
Max Lewis <max.a.lewis98@gmail.com>
2020-05-24 14:12:18 -0700

    expess/react server interaction functional on localhost

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 const express = require("express");
-const router = require("express").Router();
+// const router = require("express").Router();
 
-const routes = require("./routes");
+// const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "/client/build/index.html"));
-});
+// router.use(function(req, res) {
+//   res.sendFile(path.join(__dirname, "/client/build/index.html"));
+// });
 
 
 // Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+app.listen(PORT, function () {
+  console.log(`Hosting on http://localhost:${PORT}/`);
 });
